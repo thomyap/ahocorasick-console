1e4b03005e7955434f7430a41579c1fec0e99b64
Thomas Yap <thomyap02@gmail.com>
2018-07-25 10:10:14 +0700

    first commit

---
+++ testaho/AhoCorasick.Tests.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+
+namespace AhoCorasick
+{
+    public class Tests
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            string text = "hello and welcome to this beautiful world!";
+
+            AhoCorasick.Trie trie = new AhoCorasick.Trie();
+            trie.Add("hello");
+            trie.Add("world");
+            trie.Build();
+
+            string[] matches = trie.Find(text).ToArray();
+
+            Assert.AreEqual(2, matches.Length);
+            Assert.AreEqual("hello", matches[0]);
+            Assert.AreEqual("world", matches[1]);
+        }
+
+        [Test]
+        public void Contains()
+        {
+            string text = "hello and welcome to this beautiful world!";
+
+            AhoCorasick.Trie trie = new AhoCorasick.Trie();
+            trie.Add("hello");
+            trie.Add("world");
+            trie.Build();
+
+            Assert.IsTrue(trie.Find(text).Any());
+        }
+
+        [Test]
+        public void LineNumbers()
+        {
+            string text = "world, i hello you!";
+            string[] words = new[] { "hello", "world" };
+
+            AhoCorasick.Trie<int> trie = new AhoCorasick.Trie<int>();
+            for (int i = 0; i < words.Length; i++)
+                trie.Add(words[i], i);
+            trie.Build();
+
+            int[] lines = trie.Find(text).ToArray();
+
+            Assert.AreEqual(2, lines.Length);
+            Assert.AreEqual(1, lines[0]);
+            Assert.AreEqual(0, lines[1]);
+        }
+
+        [Test]
+        public void Words()
+        {
+            string[] text = "one two three four".Split(' ');
+            
+            AhoCorasick.Trie<string, bool> trie = new AhoCorasick.Trie<string, bool>();
+            trie.Add(new[] { "three", "four" }, true);
+            trie.Build();
+
+            Assert.IsTrue(trie.Find(text).Any());
+        }
+    }
+}
---
+++ testaho/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using NUnit.Framework;
+using ManagedCuda;
+
+
+namespace AhoCorasickTree
+{
+    class Program
+    {
+        static CudaKernel addWithCuda;
+
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            //untuk true / false
+
+            string[] text = "ini laptop thomas".Split(' ');
+
+            AhoCorasick.Trie<string, bool> trie = new AhoCorasick.Trie<string, bool>();
+            trie.Add(new[] { "thomasa" }, true);
+            trie.Build();
+
+            /*
+            string[] masuk = "one two three four".Split(' ');
+
+            AhoCorasick.Trie<string, bool> trie = new AhoCorasick.Trie<string, bool>();
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\Thomas Yap\Documents\CUDA Examples\testaho\lorem 1000words.txt");
+            
+            trie.Build();
+            */
+            Console.WriteLine(trie.Find(text).Any());
+            
+
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+            Console.ReadKey();
+
+
+        }
+    }
+}
